a34e0132366be9c50f8ad90bcb1f11803f0e21da
Vladimir Abramyants <vabramanc@gmaill.com>
2019-05-08 11:31:59 +0300

    created new views

---
+++ src/views/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <GroupList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GroupList from '@/components/GroupList.vue'
+
+export default {
+  name: 'home',
+  components: {
+    GroupList
+  }
+}
+</script>
