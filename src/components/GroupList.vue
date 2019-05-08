5807e5eea9315e448bd61746881433cca0051d0a
Vladimir Abramyants <vabramanc@gmaill.com>
2019-05-08 11:32:37 +0300

    Create new component

---
+++ src/components/GroupList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <p>hey</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GroupList'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+p{
+  color: $color;
+}
+</style>
