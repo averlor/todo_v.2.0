d48d8636ec056d1b123e408c5e8d9d4a2442e765
Vladimir Abramyants <vabramanc@gmaill.com>
2019-05-08 11:33:54 +0300

    Edit way for router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
+import TodoList from './views/TodoList.vue'
+import TaskList from './views/TaskList.vue'
+import TaskEdit from './views/TaskEdit.vue'
 
 Vue.use(Router)
 
@@ -8,16 +11,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'TodoList',
+      component: TodoList
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/:groupName',
+      name: 'groupName',
+      component: TaskList
+    },
+    {
+      path: '/:groupName/:taskName/edit',
+      name: 'taskName',
+      component:TaskEdit
     }
   ]
 })
